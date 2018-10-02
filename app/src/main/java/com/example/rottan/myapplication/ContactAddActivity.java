5cce9a857e9b04bbd2fb25b68b40f63b97006576
Klimek.Lukasz <Rottanek@gmail.com>
2018-10-02 19:20:14 +0200

    New Buttons

--- app/src/main/java/com/example/rottan/myapplication/ContactAddActivity.java
+++ app/src/main/java/com/example/rottan/myapplication/ContactAddActivity.java
@@ -1,9 +1,10 @@
 package com.example.rottan.myapplication;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -11,6 +12,7 @@ import android.widget.Toast;
 
 public class ContactAddActivity extends AppCompatActivity {
 
+    Button btnBack;
     EditText etName;
     EditText etAdress;
     EditText etWebsite;
@@ -22,18 +24,18 @@ public class ContactAddActivity extends AppCompatActivity {
     ImageView ivHappy;
     ImageView ivNormal;
     ImageView ivSad;
-    Integer emotion;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_add);
 
+        btnBack = findViewById(R.id.btnBaack);
 
         etWebsite = findViewById(R.id.etWeb);
         etName = findViewById(R.id.etName);
         etAdress = findViewById(R.id.etAdress);
-
 
 
         etNumber = findViewById(R.id.etNumber);
@@ -51,31 +53,28 @@ public class ContactAddActivity extends AppCompatActivity {
                 if (etName.getText().toString().isEmpty() ||
                         etWebsite.getText().toString().isEmpty() ||
                         etAdress.getText().toString().isEmpty() ||
-                        etNumber.getEditableText().toString().isEmpty())
-                {
+                        etNumber.getEditableText().toString().isEmpty()) {
 
-                    Toast.makeText(ContactAddActivity.this,"please enter All fields",Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    String emotion ="happy";
+                    Toast.makeText(ContactAddActivity.this, "please enter All fields", Toast.LENGTH_SHORT).show();
+                } else {
+                    String emotion = "happy";
                     String name = etName.getText().toString().trim();
                     String website = etWebsite.getText().toString().trim();
                     String adres = etAdress.getText().toString().trim();
                     String number = etNumber.getText().toString().trim();
 
-                    intent.putExtra("name",name);
-                    intent.putExtra("website",website);
-                    intent.putExtra("adres",adres);
-                    intent.putExtra("number",number);
+                    intent.putExtra("name", name);
+                    intent.putExtra("website", website);
+                    intent.putExtra("adres", adres);
+                    intent.putExtra("number", number);
                     intent.putExtra("emotion", emotion);
 
-                    setResult(RESULT_OK,intent);
+                    setResult(RESULT_OK, intent);
                     ContactAddActivity.this.finish();
                 }
             }
         });
 ////////////////////////////////////////////
-
 
 
         ivNormal.setOnClickListener(new View.OnClickListener() {
@@ -86,25 +85,23 @@ public class ContactAddActivity extends AppCompatActivity {
                 if (etName.getText().toString().isEmpty() ||
                         etWebsite.getText().toString().isEmpty() ||
                         etAdress.getText().toString().isEmpty() ||
-                        etNumber.getEditableText().toString().isEmpty())
-                {
+                        etNumber.getEditableText().toString().isEmpty()) {
 
-                    Toast.makeText(ContactAddActivity.this,"please enter All fields",Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    String emotion ="neutral";
+                    Toast.makeText(ContactAddActivity.this, "please enter All fields", Toast.LENGTH_SHORT).show();
+                } else {
+                    String emotion = "neutral";
                     String name = etName.getText().toString().trim();
                     String website = etWebsite.getText().toString().trim();
                     String adres = etAdress.getText().toString().trim();
                     String number = etNumber.getText().toString().trim();
 
-                    intent.putExtra("name",name);
-                    intent.putExtra("website",website);
-                    intent.putExtra("adres",adres);
-                    intent.putExtra("number",number);
+                    intent.putExtra("name", name);
+                    intent.putExtra("website", website);
+                    intent.putExtra("adres", adres);
+                    intent.putExtra("number", number);
                     intent.putExtra("emotion", emotion);
 
-                    setResult(RESULT_OK,intent);
+                    setResult(RESULT_OK, intent);
                     ContactAddActivity.this.finish();
                 }
             }
@@ -119,29 +116,38 @@ public class ContactAddActivity extends AppCompatActivity {
                 if (etName.getText().toString().isEmpty() ||
                         etWebsite.getText().toString().isEmpty() ||
                         etAdress.getText().toString().isEmpty() ||
-                        etNumber.getEditableText().toString().isEmpty())
-                {
+                        etNumber.getEditableText().toString().isEmpty()) {
 
-                    Toast.makeText(ContactAddActivity.this,"please enter All fields",Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    String emotion ="sad";
+                    Toast.makeText(ContactAddActivity.this, "please enter All fields", Toast.LENGTH_SHORT).show();
+                } else {
+                    String emotion = "sad";
                     String name = etName.getText().toString().trim();
                     String website = etWebsite.getText().toString().trim();
                     String adres = etAdress.getText().toString().trim();
                     String number = etNumber.getText().toString().trim();
 
-                    intent.putExtra("name",name);
-                    intent.putExtra("website",website);
-                    intent.putExtra("adres",adres);
-                    intent.putExtra("number",number);
+                    intent.putExtra("name", name);
+                    intent.putExtra("website", website);
+                    intent.putExtra("adres", adres);
+                    intent.putExtra("number", number);
                     intent.putExtra("emotion", emotion);
 
-                    setResult(RESULT_OK,intent);
+                    setResult(RESULT_OK, intent);
                     ContactAddActivity.this.finish();
                 }
             }
         });
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setResult(RESULT_CANCELED);
+                ContactAddActivity.this.finish();
+
+            }
+        });
+
+
 
     }
 }
--- app/src/main/java/com/example/rottan/myapplication/MainActivity.java
+++ app/src/main/java/com/example/rottan/myapplication/MainActivity.java
@@ -2,23 +2,22 @@ package com.example.rottan.myapplication;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
 
 
+    final int ContactAddActivity = 2;
     TextView tvContactsApp;
     Button btnCreateNewContact;
-    final int ContactAddActivity = 2;
-
+    Button btnClose;
     ImageView ivMood;
     TextView tvPhone;
     TextView tvAdres;
@@ -37,15 +36,17 @@ public class MainActivity extends AppCompatActivity {
                 tvAdres.setText(data.getStringExtra("adres"));
                 tvAdres.setVisibility(View.VISIBLE);
                 ivMood.setVisibility(View.VISIBLE);
-                if (data.getStringExtra("emotion").equals("happy")) {
-                    ivMood.setImageResource(R.drawable.ic_mood_black_24dp);
-                } else if (data.getStringExtra("emotion").equals("neutral")) {
 
-                    ivMood.setImageResource(R.drawable.ic_sentiment_neutral_black_24dp);
-
-                } else
-                {
-                    ivMood.setImageResource(R.drawable.ic_mood_bad_black_24dp);
+                switch (data.getStringExtra("emotion")) {
+                    case "happy":
+                        ivMood.setImageResource(R.drawable.ic_mood_black_24dp);
+                        break;
+                    case "neutral":
+                        ivMood.setImageResource(R.drawable.ic_sentiment_neutral_black_24dp);
+                        break;
+                    default:
+                        ivMood.setImageResource(R.drawable.ic_mood_bad_black_24dp);
+                        break;
                 }
             }
 
@@ -60,53 +61,62 @@ public class MainActivity extends AppCompatActivity {
         tvContactsApp = findViewById(R.id.tvContactsApp);
         btnCreateNewContact = findViewById(R.id.btnCreateNewContact);
 
+
         ivMood = findViewById(R.id.ivMood);
         tvAdres = findViewById(R.id.tvAdres);
         tvPhone = findViewById(R.id.tvPhone);
         tvWeb = findViewById(R.id.tvWebsite);
+        btnClose = findViewById(R.id.btnClose);
 
 
-     btnCreateNewContact.setOnClickListener(new View.OnClickListener() {
-         @Override
-         public void onClick(View v) {
-             Intent intent = new Intent(MainActivity.this,
-                     com.example.rottan.myapplication.ContactAddActivity.class  );
+        btnCreateNewContact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,
+                        com.example.rottan.myapplication.ContactAddActivity.class);
 
-             startActivityForResult(intent,ContactAddActivity);
-         }
-
-
-     });
+                startActivityForResult(intent, ContactAddActivity);
+            }
 
 
-     tvAdres.setOnClickListener(new View.OnClickListener() {
-         @Override
-         public void onClick(View v) {
-
-             Intent intent = new Intent(Intent.ACTION_VIEW,Uri.parse("geo:0,0?q=" + tvAdres.getText().toString()));
-             startActivity(intent);
-         }
-     });
+        });
 
 
-     tvWeb.setOnClickListener(new View.OnClickListener() {
-         @Override
-         public void onClick(View v) {
+        tvAdres.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-             Intent intent = new Intent(Intent.ACTION_VIEW,Uri.parse("http://www." + tvWeb.getText().toString()));
-             startActivity(intent);
-         }
-     });
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=" + tvAdres.getText().toString()));
+                startActivity(intent);
+            }
+        });
 
-     tvPhone.setOnClickListener(new View.OnClickListener() {
-         @Override
-         public void onClick(View v) {
 
-            // Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse(tvPhone.getText().toString()));
-             Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + tvPhone.getText().toString()) );
-             startActivity(intent);
-         }
-     });
+        tvWeb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www." + tvWeb.getText().toString()));
+                startActivity(intent);
+            }
+        });
+
+        tvPhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse(tvPhone.getText().toString()));
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + tvPhone.getText().toString()));
+                startActivity(intent);
+            }
+        });
+
+        btnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.exit(0);
+            }
+        });
 
 
     }
